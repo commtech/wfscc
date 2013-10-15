82c370d795da4ffa37e04b9dfc62d64819670019
William Fagan <willf@commtech-fastcom.com>
2013-10-15 13:47:51 -0500

    Copy over Python install cfscc.dll into the build directory.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import sys
+import os
 
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-buildOptions = dict(packages = ['fscc'], excludes = [], includes = ['re'])
+if os.name == 'nt':
+    cfscc_path = (os.path.join(sys.prefix, 'DLLs\cfscc.dll'), 'cfscc.dll')
+else:
+    cfscc_path = (os.path.join(sys.prefix, 'DLLs\libcfscc.so'), 'libcfscc.so')
+
+buildOptions = dict(packages = ['fscc'], excludes = [], includes = ['re'],
+                    include_files = [cfscc_path])
     
 executables = [
     Executable('wfscc.py')
